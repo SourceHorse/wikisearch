38ab8518f539af8e48293d5ad18c34273c761cb4
Ross Scarborough <rossscarborough@gmail.com>
2017-08-24 16:09:47 -0400

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,19 +1,17 @@
 var boxVal = "";
 var results = "";
 
-$('.reload').click(function() {
-    location.reload(true);
-});
+
 //stupid iphones and their incompatibility with position:fixed
 var iOS = !!navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform);
 if (iOS === true) {
- 
   document.getElementById('navbar1').classList.add('absolute');
   $('body').css('margin-top', 0);
 } else {
    document.getElementById('navbar1').classList.add('fixed');
 }
-function test(form) {
+
+function srch(form) {
   boxVal = form.searchbox.value;
   var query = encodeURI(boxVal.toLowerCase().replace(/\b[a-z]/g,function(f){return f.toUpperCase();}));
   var output = document.getElementById('testfield');
@@ -29,19 +27,25 @@ function test(form) {
        ary.push('<a href = "' + data[3][i] + '"><ul><li class = "title">' + data[1][i] + '</li><li class = "description">' + data[2][i] + '</li></ul>');
        }; 
      $('body').css('background-color', '#333');
+          $('#search').val('');
      if ($(window).width() < 835) {
        $('#navbar1').animate({
-         height: 120,
-         padding: 5,
+         height: 100,
+         paddingTop: 5,
        }, 300);
      } else {
        $('#navbar1').animate({
          height: 65,
-         padding: 5,
+         paddingTop: 5,
+       }, 300);
+       $('#pageti').animate({
+         fontSize: '32px',
+         marginTop: 0,
        }, 300);
      };
      
      $('#results').animate({opacity: 0}, 500, 'linear', function() {  
+       window.scrollTo(0, 0);
        $('#ar').html(ary);
        $('#searchterm').html('Search Term<br><p id = "st">' + decodeURI(query) + '</p');
      });
@@ -53,6 +57,10 @@ function test(form) {
 document.querySelector('#search').addEventListener('keypress', function (e) {
     var key = e.which || e.keyCode;
     if (key === 13) { 
-      test(this.form);
+      srch(this.form);
     }
+});
+
+$('.reload').click(function() {
+    location.reload(true);
 });
